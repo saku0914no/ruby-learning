82d7ac5c6f305edd1d6e009d8a86b7c20df77d82
Goto Shino <5100914shino@gmail.com>
2025-07-01 16:14:38 +0900

    feat: loop

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,16 @@
 class Calculator
+  def self.usage
+    puts <<~End
+      計算方法 ->
+      a : 足し算
+      s : 引き算
+      m : 掛け算
+      d : 割り算
+      e : 計算を終了する
+      -------------
+    End
+  end
+
   def self.add(num1, num2)
     num1 + num2
   end
@@ -16,4 +28,27 @@ class Calculator
   end
 end
 
-puts Calculator.add(1, 2)
+loop do
+  puts '一つ目の値を入力してください ->'
+  num1 = ($stdin.gets.chomp).to_i
+  puts '二つ目の値を入力してください ->'
+  num2 = ($stdin.gets.chomp).to_i
+
+  Calculator.usage
+  method = $stdin.gets.chomp
+
+  case method
+  when 'a'
+    p Calculator.add(num1, num2)
+  when 's'
+    p Calculator.substract(num1, num2)
+  when 'm'
+    p Calculator.multiply(num1, num2)
+  when 'd'
+    p Calculator.divide(num1, num2)
+  when 'e'
+    exit
+  else
+    Calculator.usage
+  end
+end
